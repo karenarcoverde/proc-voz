8be51c3f02a78988fb3c3508d5252b2f2c84da1f
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2023-10-02 23:06:10 -0300

    Add files via upload

--- modulationgas.py
+++ modulationgas.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sounddevice as sd
 import librosa
+import soundfile as sf
 
 def transform_to_helium_voice(input_path, output_path):
     # Carregando o áudio
@@ -12,17 +13,16 @@ def transform_to_helium_voice(input_path, output_path):
     # Convertendo o áudio modificado para um formato que o sounddevice pode reproduzir
     sounddevice_array = np.array(y_pitch_shifted * (2**15), dtype=np.int16)
 
+    # Salvando o áudio modificado usando soundfile
+    sf.write(output_path, y_pitch_shifted, sr)
+
     # Reproduzindo o áudio usando sounddevice
     sd.play(sounddevice_array, samplerate=sr)
 
     # Esperando o áudio terminar de tocar
     sd.wait()
 
-    # Salvando o áudio modificado (opcional)
-    librosa.output.write_wav(output_path, y_pitch_shifted, sr)
 
 input_audio_file = "Patinho.wav"
 output_audio_file = "path_to_your_output_audio_helium.wav"
 transform_to_helium_voice(input_audio_file, output_audio_file)
-
-
